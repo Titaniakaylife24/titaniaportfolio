c77532ca481ba9fc1f04e00208ec27151ca50664
Titaniakaylife24 <titakaylife@gmail.com>
2026-05-23 21:06:17 +0700

    Update portfolio

--- script.js
+++ script.js
@@ -697,3 +697,18 @@ window.addEventListener("click", function(e){
 
 });
 
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+
+    });
+
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+
+hiddenElements.forEach((el) => observer.observe(el));
